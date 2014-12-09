119f98b15d1b27339ee701c31eeeac650100c801
KAWACHI Takashi <tkawachi@gmail.com>
2014-12-09 21:30:27 +0900

    StringFormatter for Int and String

--- app/com/github/tkawachi/play/safersession/Imports.scala
+++ app/com/github/tkawachi/play/safersession/Imports.scala
@@ -1,6 +1,6 @@
 package com.github.tkawachi.play.safersession
 
 
-trait Imports extends Implicits with DefaultFormatter
+trait Imports extends Implicits with DefaultStringFormatter
 
 object Imports extends Imports
