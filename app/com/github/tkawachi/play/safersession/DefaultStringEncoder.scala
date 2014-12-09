f9f58f5d10a4818453c0b45cd2b8e25183f084e2
KAWACHI Takashi <tkawachi@gmail.com>
2014-12-09 22:11:29 +0900

    Fixed a compile error

--- app/com/github/tkawachi/play/safersession/DefaultStringEncoder.scala
+++ app/com/github/tkawachi/play/safersession/DefaultStringEncoder.scala
@@ -5,28 +5,28 @@ trait DefaultStringEncoder {
   implicit def byteStringEncoder = new StringEncoder[Byte] {
     override def encode(a: Byte) = a.toString
     override def decode(s: String) = try Some(s.toByte) catch {
-      case e: NumberFormatException => None
+      case _: NumberFormatException => None
     }
   }
 
   implicit def shortStringEncoder = new StringEncoder[Short] {
     override def encode(a: Short) = a.toString
     override def decode(s: String) = try Some(s.toShort) catch {
-      case e: NumberFormatException => None
+      case _: NumberFormatException => None
     }
   }
 
   implicit def intStringEncoder = new StringEncoder[Int] {
     override def encode(a: Int) = a.toString
     override def decode(s: String) = try Some(s.toInt) catch {
-      case e: NumberFormatException => None
+      case _: NumberFormatException => None
     }
   }
 
   implicit def longStringEncoder = new StringEncoder[Long] {
     override def encode(a: Long) = a.toString
     override def decode(s: String) = try Some(s.toLong) catch {
-      case e: NumberFormatException => None
+      case _: NumberFormatException => None
     }
   }
 
@@ -41,21 +41,21 @@ trait DefaultStringEncoder {
   implicit def floatStringEncoder = new StringEncoder[Float] {
     override def encode(a: Float) = a.toString
     override def decode(s: String) = try Some(s.toFloat) catch {
-      case e: NumberFormatException => None
+      case _: NumberFormatException => None
     }
   }
   
   implicit def doubleStringEncoder = new StringEncoder[Double] {
     override def encode(a: Double) = a.toString
     override def decode(s: String) = try Some(s.toDouble) catch {
-      case e: NumberFormatException => None
+      case _: NumberFormatException => None
     }
   }
 
   implicit def booleanStringEncoder = new StringEncoder[Boolean] {
     override def encode(a: Boolean) = a.toString
     override def decode(s: String) = try Some(s.toBoolean) catch {
-      case IllegalArgumentException => None
+      case _: IllegalArgumentException => None
     }
   }
 
